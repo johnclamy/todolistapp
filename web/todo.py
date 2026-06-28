c42905853f000d5f39a85c21c618826113dfd27c
johnclamy <johnclamy@gmail.com>
2026-06-28 21:06:07 +0100

    setup a todo.py file  under a web folder for the todo endpoint definitions

---
+++ web/todo.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter
+from model.todo import Todo
+import data.todo as service
+
+
+router = APIRouter(prefix = '/api/v1/todos')
+
+
+@router.get('/')
+async def get_todos() -> list[Todo]:
+    todos = await service.get_todos()
+
+    return todos
+
+
+@router.post('/')
+async def add_todo(todo: Todo) -> dict[str, str]:
+    await service.add_todo(todo)
+
+    return {'message': 'Todo added successfully'}
+
+
+@router.put('/{todoId}')
+async def update_todo(todoId: str, todo: Todo) -> dict[str, str]:
+    await service.update_todo(todoId)
+
+    return {'message': 'Todo updated successfully'}
+
+
+@router.delete('/{todoId}')
+async def delete_todo(todoId: str) -> dict[str, str]:
+    await service.delete_todo(todoId)
+
+    return {'message': 'Todo deleted successfully'}
